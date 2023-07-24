08476be49118d932c0064102ba9fe81cae314140
Ibrohimbek Davronbekov <108320639+Ibrokihimbek@users.noreply.github.com>
2023-07-24 09:05:43 +0500

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:html_example/screens/html_example_page.dart';
+import 'package:html_example/screens/example_franchising.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HtmlExamplePage(),
+      home: HtmlFranchisingPage(),
     );
   }
 }
